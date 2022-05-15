ecbf14b30741d2d211903f97cc9b498b8c0a0425
Daniel Mares <danielmares32@gmail.com>
2022-05-15 13:56:06 -0500

    se actualiza

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/region1', (req, res) => {
